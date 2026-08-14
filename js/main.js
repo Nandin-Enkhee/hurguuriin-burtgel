7e0e9893deef5560fb264c0657baaddfd9c98a27
Nandin-Enkhee <enhamgalannandin@gmail.com>
2026-08-14 22:29:07 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,8 +20,7 @@ import * as Settings from './settings.js';
 loadLocal();
 
 Object.assign(window,
-  { show:Router.show, toggleSel:UI.toggleSel, chooseSel:UI.chooseSel,
-    toggleMultiSel:UI.toggleMultiSel, closeKeyboard:UI.closeKeyboard },
+  { show:Router.show, toggleSel:UI.toggleSel, chooseSel:UI.chooseSel },
   { pickToggle:Picker.pickToggle, pickSet:Picker.pickSet, pickAll:Picker.pickAll },
   Auth, Fridge, Entry, Work, Buy, Out, Receipt, Dash, Debt, Records, Salary, Settings
 );
